sezero/libtimidity

readsbk.c: change READSTR() macro into a procedure. (from timidity++).

src/readsbk.c
@@ -70,7 +70,18 @@ static int READW(uint16 *vp, FILE *fd)
 	return 1;
 }
 
-#define READSTR(var,fd)	fread(var, 20, 1, fd)
+static int READSTR(char *str, FILE *fd)
+{
+	int n;
+	if (fread(str, 20, 1, fd) != 1) return -1;
+	str[19] = '\0';
+	n = strlen(str);
+	while (n > 0 && str[n - 1] == ' ')
+		n--;
+	str[n] = '\0';
+	return n;
+}
+
 #define READID(var,fd)	fread(var, 1, 4, fd)
 #define READB(var,fd)	fread(var, 1, 1, fd)
 #define SKIPB(fd)	{uint8 dummy; fread(&dummy, 1, 1, fd);}

sezero/SDL_sound

src/timidity/readsbk.c
@@ -45,7 +45,18 @@ static int READW(Uint16 *vp, SDL_RWops *rw)
 	return 1;
 }
 
-#define READSTR(var,rw)	SDL_RWread(rw, var, 1, 20)
+static int READSTR(char *str, SDL_RWops *rw)
+{
+	int n;
+	if (SDL_RWread(rw, str, 1, 20) != 20) return -1;
+	str[19] = '\0';
+	n = (int) SDL_strlen(str);
+	while (n > 0 && str[n - 1] == ' ')
+		n--;
+	str[n] = '\0';
+	return n;
+}
+
 #define READID(var,rw)	SDL_RWread(rw, var, 1, 4)
 #define READB(var,rw)	SDL_RWread(rw, var, 1, 1)
 #define SKIPB(rw)	SDL_RWseek(rw, 1, RW_SEEK_CUR);