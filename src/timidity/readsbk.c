sezero/libtimidity

initial minimal error checking to load_sbk().
Still LOTS TO DO.

src/readsbk.c
@@ -88,6 +88,17 @@ static int READSTR(char *str, FILE *fd)
 #define SKIPW(fd)	fseek(fd, 2, SEEK_CUR)
 #define SKIPDW(fd)	fseek(fd, 4, SEEK_CUR)
 
+static long FILESIZE(FILE *fd)
+{
+	long len, pos;
+
+	pos = ftell(fd);
+	fseek(fd, 0, SEEK_END);
+	len = ftell(fd) - pos;
+	fseek(fd, pos, SEEK_SET);
+	return len;
+}
+
 static int getchunk(const char *id);
 static void process_chunk(int id, int s, SFInfo *sf, FILE *fd);
 static void load_sample_names(int size, SFInfo *sf, FILE *fd);
@@ -149,13 +160,21 @@ static void debugval(const char *tag, int v)
  * load sbk file
  *----------------------------------------------------------------*/
 
-void load_sbk(FILE *fd, SFInfo *sf)
+int load_sbk(FILE *fd, SFInfo *sf)
 {
 	tchunk chunk, subchunk;
+	long len;
 
-	READID(sf->sbkh.riff, fd);
-	READDW(&sf->sbkh.size, fd);
-	READID(sf->sbkh.sfbk, fd);
+	len = FILESIZE(fd);
+	if (len < 32) /* better?? */
+		return -1;
+
+	READCHUNK(&chunk, fd);
+	if (getchunk(chunk.id) != RIFF_ID) return -1;
+	if (chunk.size != len - 8) return -1;
+
+	READID(chunk.id, fd);
+	if (getchunk(chunk.id) != SFBK_ID) return -1;
 
 	sf->in_rom = 1;
 	while (! feof(fd)) {
@@ -168,6 +187,8 @@ void load_sbk(FILE *fd, SFInfo *sf)
 			break;
 		}
 	}
+
+	return 0;
 }
 
 

src/sbk.h
@@ -99,7 +99,7 @@ typedef struct _SFInfo {
 	int nrpgens, nrigens;
 	tgenrec *presetgen, *instgen;
 
-	tsbkheader sbkh;
+	/*tsbkheader sbkh;*/
 
 	/*char *sf_name;*/
 
@@ -114,7 +114,7 @@ typedef struct _SFInfo {
 #define load_sbk TIMI_NAMESPACE(load_sbk)
 #define free_sbk TIMI_NAMESPACE(free_sbk)
 
-void load_sbk(FILE *fp, SFInfo *sf);
+int load_sbk(FILE *fp, SFInfo *sf);
 void free_sbk(SFInfo *sf);
 
 #endif

src/sndfont.c
@@ -172,7 +172,15 @@ void init_soundfont(MidSong *song, const char *fname, int order)
 	}
 	sfrec.fname = (char*)timi_malloc(strlen(fname)+1);
 	strcpy(sfrec.fname, fname);
-	load_sbk(sfrec.fd, &sfinfo);
+	if (load_sbk(sfrec.fd, &sfinfo) < 0) {
+		DEBUG_MSG("%s: bad soundfont file\n", fname);
+		fclose(sfrec.fd);
+		sfrec.fd = NULL;
+		timi_free(sfrec.fname);
+		sfrec.fname = NULL;
+		free_sbk(&sfinfo);
+		return;
+	}
 
 	for (i = 0; i < sfinfo.nrpresets - 1; i++) {
 		int bank = sfinfo.presethdr[i].bank;

sezero/SDL_sound

src/timidity/readsbk.c
@@ -124,14 +124,20 @@ static void debugval(const char *tag, int v)
  * load sbk file
  *----------------------------------------------------------------*/
 
-void load_sbk(SDL_RWops *rw, SFInfo *sf)
+int load_sbk(SDL_RWops *rw, SFInfo *sf)
 {
 	const Sint64 rwend = SDL_RWsize(rw);
 	tchunk chunk, subchunk;
 
-	READID(sf->sbkh.riff, rw);
-	READDW(&sf->sbkh.size, rw);
-	READID(sf->sbkh.sfbk, rw);
+	if (rwend < 32) /* better?? */
+		return -1;
+
+	READCHUNK(&chunk, rw);
+	if (getchunk(chunk.id) != RIFF_ID) return -1;
+	if (chunk.size != rwend - 8) return -1;
+
+	READID(chunk.id, rw);
+	if (getchunk(chunk.id) != SFBK_ID) return -1;
 
 	sf->in_rom = 1;
 	while (SDL_RWtell(rw) < rwend) {
@@ -144,6 +150,8 @@ void load_sbk(SDL_RWops *rw, SFInfo *sf)
 			break;
 		}
 	}
+
+	return 0;
 }
 
 

src/timidity/sbk.h
@@ -78,7 +78,7 @@ typedef struct _SFInfo {
 	int nrpgens, nrigens;
 	tgenrec *presetgen, *instgen;
 
-	tsbkheader sbkh;
+	/*tsbkheader sbkh;*/
 
 	/*char *sf_name;*/
 
@@ -90,7 +90,7 @@ typedef struct _SFInfo {
  * functions
  *----------------------------------------------------------------*/
 
-void load_sbk(SDL_RWops *rw, SFInfo *sf);
+int load_sbk(SDL_RWops *rw, SFInfo *sf);
 void free_sbk(SFInfo *sf);
 
 #endif

src/timidity/sndfont.c
@@ -145,7 +145,15 @@ void init_soundfont(MidiSong *song, const char *fname, int order)
 		return;
 	}
 	sfrec.fname = SDL_strdup(fname);
-	load_sbk(sfrec.rw, &sfinfo);
+	if (load_sbk(sfrec.rw, &sfinfo) < 0) {
+		SNDDBG(("%s: bad soundfont file\n", fname));
+		SDL_RWclose(sfrec.rw);
+		sfrec.rw = NULL;
+		SDL_free(sfrec.fname);
+		sfrec.fname = NULL;
+		free_sbk(&sfinfo);
+		return;
+	}
 
 	for (i = 0; i < sfinfo.nrpresets - 1; i++) {
 		int bank = sfinfo.presethdr[i].bank;